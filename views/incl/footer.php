1505c33925c5a8feab1494c6b8889b9710ac7f72
Daniel Spandel <daniel.spandel1@gmail.com>
2013-10-18 16:49:30 +0200

    adding css and js by calling functions from header and footer

--- views/incl/footer.php
+++ views/incl/footer.php
@@ -3,33 +3,8 @@
       <footer>
         <p><?=$metalPet['copyright']?></p>
       </footer>
-    </div> <!-- /container -->
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo $metalPet['jquery_url'] ?>"></script>
-    <?php
-        if($metalPet['use_bootstrap']) {
-            ?>
-             <script src="<?php echo $metalPet['bootstrap_js'] ?>"></script>  
-            <?php
-        }    
-    	//Custom js for this theme
-    	if (is_file($metalPet['theme_server_url'].'js/main.js')) {
-    		echo "<!-- Custom js for this page -->\n".
-                 "<script src='".$metalPet['theme_url']."js/main.js'></script>\n";
-    			//"<link href=\"". $metalPet['theme_url']."js/main.js \" rel=\"stylesheet\">\n";
-    	}
-    	//Custom js for this page
-    	if (is_file(_VIEW_SERVER_URL_.'js/'.$metalPet['page'].'.js')) {
-    		echo "<!-- Custom js for this page -->\n".
-                 "<script src='".$metalPet['theme_url']."js/".$metalPet['page'].".js'></script>\n";
-    			//"<link href=\"". _VIEW_URL_."js/". $metalPet['page'] .".js \" rel=\"stylesheet\">\n";
-    	}
-        if ($metalPet['debug']) {
-            echo "<pre>".$metalPet['log']."\t==END OF LOG==\t\n"."</pre>";
-        }
-    ?>
+    </div> 
+    <!-- /container -->
+    <?=include_js_after()?>
   </body>
 </html>
--- views/incl/header.php
+++ views/incl/header.php
@@ -7,56 +7,27 @@
 		<meta name="description" content="">
 		<meta name="author" content="">
 		<title><?=$metalPet['title']?></title>
-
-		<?php
-        if($metalPet['use_bootstrap']) {
-            ?>
-		<!-- Bootstrap core CSS -->
-		<link href="<?php echo $metalPet['bootstrap_css'] ?>" rel="stylesheet">
-
-		<?php 
-		}
-			//Custom css for this theme
-			if(is_file($metalPet['theme_server_url'].'css/style.css')) {
-				echo "<!-- Custom css for this theme -->\n".
-				"<link href=\"". $metalPet['theme_url']."css/style.css \" rel=\"stylesheet\">\n";
-			}
-			//Custom css for this page
-			if(is_file(_VIEW_SERVER_URL_.'css/'.$metalPet['page'].'.css')) {
-				echo "<!-- Custom css for this page -->\n".
-					"<link href=\"". _VIEW_URL_."css/". $metalPet['page'] .".css \" rel=\"stylesheet\">\n";
-			}
-		?>
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-			<script src="<?php echo _BOOTSTRAP_URL_ ; ?>docs-assets/js/html5shiv.js"></script>
-			<script src=".<?php echo _BOOTSTRAP_URL_ ; ?>docs-assets/js/respond.min.js"></script>
-		<![endif]-->
+		<?=include_css()?>
+		<?=include_js_before()?>
 	</head>
 	 <body>
 		<div class="navbar navbar-inverse navbar-fixed-top">
 			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
+				<div class="navbar-header">					
 					<a class="navbar-brand" href="<?=_HTTP_URL_?>home/"><?=$metalPet['site_name']?></a>
 				</div>
 				<div class="navbar-collapse collapse">
 				<?php 
-				if(isset($metalPet['menu']) && is_array($metalPet['menu'])) {
-					echo createMenu($metalPet['menu'], $metalPet['page'], $metalPet['rewrite_queries']);
-				}
-				if(isset($metalPet['login']) && $metalPet['login']==true) {
-					echo createLogin();					
-				}
-				if(isset($metalPet['searchbar']) && $metalPet['searchbar']==true) {
-					echo createSearchbar();
-				}				
-				?>
-					
+					if(isset($metalPet['menu']) && is_array($metalPet['menu'])) {
+						echo createMenu($metalPet['menu'], $metalPet['page'], $metalPet['rewrite_queries']);
+					}
+					if(isset($metalPet['login']) && $metalPet['login']==true) {
+						echo createLogin();
+					}
+					if(isset($metalPet['searchbar']) && $metalPet['searchbar']==true) {
+						echo createSearchbar();
+					}				
+				?>					
 				</div><!--/.navbar-collapse -->
 			</div>
 		</div>
